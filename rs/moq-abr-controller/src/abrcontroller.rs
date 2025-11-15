68bed4b34b312e1d7718bec8252fbc7264c3a7e3
pellju <juho.64@hotmail.com>
2025-11-15 13:49:16 +0200

    Adding changes to abrcontroller to allow setting pre-defined bitrates from client for testing

--- rs/moq-abr-controller/src/abrcontroller.rs
+++ rs/moq-abr-controller/src/abrcontroller.rs
@@ -7,6 +7,8 @@ use gstreamer;
 use gstreamer::prelude::ElementExt;
 use gstreamer::prelude::GstBinExt;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use anyhow::Context;
 
 pub struct Abrcontroller {
@@ -43,6 +45,12 @@ impl Abrcontroller {
 		pipeline.set_state(gstreamer::State::Playing);
 
 		tracing::info!("(abrcontroller.rs) Entering loop to request info what to do with the stream");
+
+		// Adding a vector for timestamps and set bitrates
+		let mut changedBitrates: Vec<(u64,u32)> = vec![];
+		// Setting the previous changed value to be used for comparing the bitrate, 8000 kbps as the default
+		let mut previousValue: u32 = 8000;
+
 		loop {
 			match self.track.next_group().await {
 				Ok(Some(mut group)) => {
@@ -62,6 +70,38 @@ impl Abrcontroller {
 						const newValue:u32 = 1000;
 						encoder.set_property("bitrate", &newValue);
 						//encoder.set_property("bitrate", 1000);
+					} else {
+						// Parse input from a text
+						let splitted: Vec<&str> = base.split(";").collect();
+						let key = splitted[0];
+						let value = splitted[1];
+
+						if key == "abr" {
+							tracing::info!("[moq-abr-controller] Adjusting the bitrate to {}!", value);
+							let mut newValue:u32 = value.parse().unwrap();
+
+							let currentTime = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+							// 0 cannot be set as the bitrate, must use 1 at least
+							// Also, checking if value is not over 10000 [kbps]
+							if newValue == 0 {
+								newValue = 1;
+							} else if newValue > 10000 {
+								newValue = 10000
+							}
+
+							let timestampAndNewvalue = (currentTime, newValue);
+							changedBitrates.push(timestampAndNewvalue);
+
+							if newValue != previousValue {
+								tracing::info!("{:?}",changedBitrates);
+								encoder.set_property("bitrate", newValue);
+								tracing::info!("[moq-abr-controller] The new value has been set!");
+							} else {
+								tracing::info!("[moq-abr-controller] The newly given bitrate is the same than the previous one");
+							}
+						}
+
 					}
 				}
 
